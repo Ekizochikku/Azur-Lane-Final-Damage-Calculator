7c34f80b9181b31ef6e1ccf2ab082de502776bea
Ekizochikku <b.khang12@yahoo.com>
2019-06-20 15:29:57 -0700

    Add files via upload

---
+++ mainCalc.java
@@ -0,0 +1,129 @@
+package azureLaneCalc;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.ButtonGroup;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+
+public class mainCalc extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainCalc frame = new mainCalc();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public mainCalc() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JComboBox shipName = new JComboBox();
+		
+		JComboBox comboBox_1 = new JComboBox();
+		JComboBox comboBox_2 = new JComboBox();
+		
+		//The checkbox button for the user to determine if it's a critical hit, and first salvo
+		JCheckBox isCritical = new JCheckBox("Critical Hit");
+		JCheckBox isFirstSalvo = new JCheckBox("First Salvo");
+
+		
+		
+		//Button group for the weapon damage types
+		JRadioButton bulletType1 = new JRadioButton("PLACEHOLDER");
+		JRadioButton bulletType2 = new JRadioButton("PLACEHOLDER");
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(bulletType1);
+		group.add(bulletType2);
+		
+		/*Button to determine final calculation
+		* For now does nothing as Subject to change 
+		* we could have it change to entire screen
+		* or have the calculated damage number appear on the right side
+		*/
+		JButton calculateButton = new JButton("Calculate");
+			
+		
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(163)
+					.addComponent(calculateButton)
+					.addContainerGap(277, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(isFirstSalvo)
+							.addContainerGap())
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(isCritical)
+								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(bulletType1)
+								.addGap(18)
+								.addComponent(bulletType2)
+								.addGap(28))
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+									.addComponent(comboBox_1, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(shipName, Alignment.LEADING, 0, 60, Short.MAX_VALUE))
+								.addPreferredGap(ComponentPlacement.UNRELATED)
+								.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap(279, Short.MAX_VALUE)))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(shipName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 78, Short.MAX_VALUE)
+					.addComponent(isFirstSalvo)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(isCritical)
+						.addComponent(bulletType2)
+						.addComponent(bulletType1))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(calculateButton)
+					.addGap(21))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	
+}
