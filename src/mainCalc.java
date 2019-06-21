ac3bd8526549f0ca97c9a2dff59bf1e21f3129b8
Black <Black@192.168.0.11>
2019-06-21 08:02:36 -0700

    Just the additional buttons, next commit will be the dropdown list items

--- src/mainCalc.java
+++ src/mainCalc.java
@@ -1,4 +1,4 @@
-package azureLaneCalc;
+
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -15,6 +15,9 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JList;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 public class mainCalc extends JFrame {
 
@@ -47,9 +50,15 @@ public class mainCalc extends JFrame {
 		setContentPane(contentPane);
 		
 		JComboBox shipName = new JComboBox();
+		JComboBox weaponNames = new JComboBox();
 		
-		JComboBox comboBox_1 = new JComboBox();
-		JComboBox comboBox_2 = new JComboBox();
+		JComboBox shipList = new JComboBox();
+		JComboBox weaponType = new JComboBox();
+		JComboBox currentWorld = new JComboBox();
+		
+		JComboBox enemyName = new JComboBox();
+		JComboBox dangerLevel = new JComboBox();
+			
 		
 		//The checkbox button for the user to determine if it's a critical hit, and first salvo
 		JCheckBox isCritical = new JCheckBox("Critical Hit");
@@ -58,8 +67,8 @@ public class mainCalc extends JFrame {
 		
 		
 		//Button group for the weapon damage types
-		JRadioButton bulletType1 = new JRadioButton("PLACEHOLDER");
-		JRadioButton bulletType2 = new JRadioButton("PLACEHOLDER");
+		JRadioButton bulletType1 = new JRadioButton("HE");
+		JRadioButton bulletType2 = new JRadioButton("AP");
 		
 		ButtonGroup group = new ButtonGroup();
 		group.add(bulletType1);
@@ -71,59 +80,80 @@ public class mainCalc extends JFrame {
 		* or have the calculated damage number appear on the right side
 		*/
 		JButton calculateButton = new JButton("Calculate");
-			
 		
+		JLabel lblAmmoType = new JLabel("Ammo Type");
 		
+	
+		
+		/*
+		 * @author: Kevin Nguyen (kvn96)
+		 * creating the gui design with the proper buttons
+		 */
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(163)
+					.addGap(165)
 					.addComponent(calculateButton)
-					.addContainerGap(277, Short.MAX_VALUE))
+					.addContainerGap(180, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(isFirstSalvo)
-							.addContainerGap())
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-							.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(shipName, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addComponent(weaponType, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(weaponNames, Alignment.TRAILING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(shipList, Alignment.TRAILING, 0, 60, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(currentWorld, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED, 80, Short.MAX_VALUE)
+									.addComponent(enemyName, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addComponent(lblAmmoType)
+									.addComponent(dangerLevel, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addComponent(isCritical)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(bulletType1)
-								.addGap(18)
-								.addComponent(bulletType2)
-								.addGap(28))
-							.addGroup(gl_contentPane.createSequentialGroup()
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(comboBox_1, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(shipName, Alignment.LEADING, 0, 60, Short.MAX_VALUE))
-								.addPreferredGap(ComponentPlacement.UNRELATED)
-								.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(279, Short.MAX_VALUE)))))
+								.addComponent(isFirstSalvo))
+							.addPreferredGap(ComponentPlacement.RELATED, 200, Short.MAX_VALUE)
+							.addComponent(bulletType1)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(bulletType2)))
+					.addGap(72))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(shipName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 78, Short.MAX_VALUE)
-					.addComponent(isFirstSalvo)
+						.addComponent(shipName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(shipList, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(currentWorld, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(enemyName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(20)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(weaponType, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(weaponNames, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+						.addComponent(dangerLevel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 75, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(isFirstSalvo)
+						.addComponent(lblAmmoType))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(isCritical)
-						.addComponent(bulletType2)
-						.addComponent(bulletType1))
+						.addComponent(bulletType1)
+						.addComponent(bulletType2))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(calculateButton)
-					.addGap(21))
+					.addGap(20))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
-	
 }
+
