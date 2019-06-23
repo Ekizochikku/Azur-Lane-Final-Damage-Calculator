3f9fe738e112f86325b991f1110bcb07b9df795a
Brian <Brian@10.72.108.122>
2019-06-22 17:02:30 -0700

    More smaller methods

--- src/GUIutil.java
+++ src/GUIutil.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
+ * @author Brian Khang (Ekizochikku)
  * Methods for the GUI to call. 
  */
 public class GUIutil {
 	
 	/*
-	 * @author: Brian Khang (Ekizochikku)
 	 * Determines which ship file needs to be opened and read.
+	 * Returns a list that contains all ships of a certain ship type.
 	 */
 	public ArrayList<String> getShipList(String shiptype) throws FileNotFoundException, IOException {
 		String shipFile = checkShipFile(shiptype);
@@ -23,8 +24,8 @@ public class GUIutil {
 	}
 	
 	/*
-	 * @author: Brian Khang (Ekizochikku)
 	 * Determines which weapon file needs to be opened and read.
+	 * Returns a list that contains all weapons of a certain weapon type.
 	 */
 	public ArrayList<String> getWeaponList(String weptype) throws FileNotFoundException, IOException {
 		String wepFile = checkWepFile(weptype);
@@ -34,8 +35,8 @@ public class GUIutil {
 	}
 	
 	/*
-	 * @author Brian Khang (Ekizochikku)
 	 * Check which file needs to be opened for ships.
+	 * Returns a string containing the name of the file.
 	 */
 	public String checkShipFile(String shiptype) {
 		String shipFile = "";
@@ -75,8 +76,8 @@ public class GUIutil {
 	}
 	
 	/*
-	 * @author Brian Khang (Ekizochikku)
 	 * Checks which weapon file to open.
+	 * Returns a string containing the name of the file.
 	 */
 	public String checkWepFile(String weptype) {
 		String wepFile = "";
@@ -101,7 +102,27 @@ public class GUIutil {
 	}
 	
 	/*
-	 * @author Brian Khang (Ekizochikku)
+	 * Returns a ship's lvl 120 parameters
+	 */
+	public ArrayList<String> getShipParams(String shiptype, String shipname) throws FileNotFoundException, IOException {
+		ArrayList<String> theParams = new ArrayList<String>();
+		String theFile = checkShipFile(shiptype);
+		BufferedReader br = new BufferedReader(new FileReader(theFile));
+		String line = br.readLine(); //Skip Header Line
+		while ((line = br.readLine()) != null && !line.isEmpty()) {
+			String[] fields = line.split(",");
+			if (fields[0].equals(shipname)) {
+				for (int i = 0; i < fields.length; i++) {
+					theParams.add(fields[i]);
+				}
+			}
+			
+		}
+		br.close();
+		return theParams;
+	}
+	
+	/*
 	 * Returns an array list containing the names of ships/weapons
 	 */
 	public ArrayList<String> getEntityNames(String theFile) throws FileNotFoundException, IOException {
