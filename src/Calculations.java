36b6d0b8ca579d56bb8fdc02b859cb165594c929
Brian <Brian@10.72.108.122>
2019-06-30 16:10:59 -0700

    Added Calculation file and enemy test file.

---
+++ src/Calculations.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/*
+ * Class that holds methods to begin calculations.
+ */
+public class Calculations {
+	
+	
+	
+//	public float getFinalDamage(String shipName, String wepName, int shipSlot, ArrayList<String> skillList) {
+//		
+//	}
+}
